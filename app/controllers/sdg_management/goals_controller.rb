a76bcecbd5365db43cf86092d13c2d042987c5b9
taitus <sebastia.roig@gmail.com>
2020-11-25 15:00:01 +0100

    Add cancancan to SDG content
    Only allow access to the SDG content section to administrators and sdg managers

--- app/controllers/sdg_management/goals_controller.rb
+++ app/controllers/sdg_management/goals_controller.rb
@@ -1,5 +1,8 @@
 class SDGManagement::GoalsController < SDGManagement::BaseController
+
+  load_and_authorize_resource class: "SDG::Goal", only: [:index]
+
   def index
-    @goals = SDG::Goal.order(:code)
+    @goals = @goals.order(:code)
   end
 end
